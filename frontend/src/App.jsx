8cdca41e85bfa58cea9a48a0d410fc423c521dbc
liuhu016 <liuhu016@realsee.com>
2024-12-18 07:06:32 +0800

    feat:command list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,20 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { ModifyServer, GetServers, RemoveServer, ModifyCommand, GetCommands, RemoveCommand } from "../wailsjs/go/main/App";
 import { Button, Card, Row, Col, Modal, Form, Input, Radio, Flex, Table, Space, message } from "antd";
-import { UnorderedListOutlined, PlusOutlined } from "@ant-design/icons";
+import { UnorderedListOutlined, PlusOutlined, ArrowUpOutlined } from "@ant-design/icons";
 
 
 
 function App() {
     const [showAddServer, setShowAddServer] = useState(false);
     const [showServerList, setShowServerList] = useState(false);
-    
+   // const [showAddCommand, setShowAddCommand] = useState(false);
+   const [showAddCommand, setShowAddCommand] = useState(false);
+
     const [servers, setServers] = useState([]);
     const [commands, setCommands] = useState([]);
     const [form] = Form.useForm();
+    const [form1] = Form.useForm();
     const columns = [
         {
             title: 'ServerIP',
@@ -104,6 +107,27 @@ function App() {
         }
         );
     }
+
+    async function doAddCommand() {
+        form1.validateFields().then((values) => {
+            let data = form1.getFieldsValue(true)
+            console.log(data)
+            ModifyCommand(data).then(() => {
+                getCommands().then(() => {
+                    message.info("添加成功")
+                    setShowAddCommand(false)
+                })
+            })
+        }, (errorInfo) => {
+            console.log("Failed:", errorInfo);
+        }
+        );
+    }
+
+    function execCommand(record) {
+        console.log(record)
+    }
+
     return (
         <div id="App">
             <div className="setting">
@@ -125,7 +149,7 @@ function App() {
             <Row style={{ paddingTop: "10px" }}>
                 <Col span={22} offset={1}>
                     <Card>
-                        <h3>服务器列表</h3>
+                        <h2>服务器列表</h2>
                         <Flex vertical gap="middle">
                             <Radio.Group
                                 block
@@ -136,7 +160,22 @@ function App() {
                                 {servers.map((option) => { return <Radio.Button key={option.name} value={option}>{option.name}</Radio.Button> })}
                             </Radio.Group>
                         </Flex>
-                        <h3>命令列表 <Button type="primary" size="small" shape="circle" icon={<PlusOutlined />}></Button></h3>
+                        <h2>命令列表 <Button type="primary" size="small" shape="circle" icon={<PlusOutlined />} onClick={() => setShowAddCommand(true)}></Button></h2>
+                        <Row gutter={10}>
+                            {
+                                commands.map((option) => {
+                                    return <Col span={8}>
+                                        <Card type="inner" size="small" title={option.name} hoverable={true} actions={[<Button type="link"  icon={<ArrowUpOutlined />} onClick={execCommand.bind(this, option)}>执行</Button>]} >
+                                        {
+                                            option.data.split('\n').map((item) => {
+                                                return <div>{item}</div>
+                                            })
+                                        }
+                                        </Card>
+                                    </Col>
+                                })
+                            }
+                        </Row>
                     </Card>
                 </Col>
             </Row>
@@ -174,7 +213,31 @@ function App() {
                         <Input />
                     </Form.Item>
                     <Form.Item label="Password" name="password" rules={[{ required: true, message: "Please input your password!" }]}>
-                        <Input.Password />
+                        <Input />
+                    </Form.Item>
+                </Form>
+            </Modal>
+
+            <Modal
+                title="添加Command"
+                open={showAddCommand}
+                onCancel={() => setShowAddCommand(false)}
+                onOk={doAddCommand}
+            >
+                <Form
+                    name="basic1"
+                    form={form1}
+                    labelCol={{ span: 5 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ name: 'command-name', data: '' }}
+                    style={{ marginTop: '30px' }}
+                    autoComplete="off"
+                >
+                    <Form.Item label="name" name="name" rules={[{ required: true, message: "Please input name!" }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="data" name="data" rules={[{ required: true, message: "Please input your port!" }]}>
+                        <Input.TextArea rows={10}/>
                     </Form.Item>
                 </Form>
             </Modal>
